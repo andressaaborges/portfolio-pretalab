a34f27be74745012491aef4a5f2cddfe456788e3
andressaaborges <emylleaborges@gmail.com>
2022-07-21 18:28:20 -0300

    feat: add icons in mobile menu

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let menu = false;
 let divMobileMenu = document.createElement('div');
+let mobileBtn = document.querySelector('#mobile-btn');
+
 function createMenuMobile() {
     let nav = document.querySelector('nav');
     let menuList = document.querySelector('#list-menu-id');
     if (menu) {
         divMobileMenu.className = 'closed';
+        mobileBtn.innerHTML = 'MENU <i class="fas fa-bars estilo-icon"></i>';
         menu = false;
     } else {
         divMobileMenu.id = 'mobile-menu';
@@ -12,6 +15,7 @@ function createMenuMobile() {
         divMobileMenu.appendChild(menuList);
         menuList.className = 'list-mobile-menu';
         divMobileMenu.className = 'opened';
+        mobileBtn.innerHTML = 'FECHAR <i class="fas fa-times estilo-icon"></i>';
         menu = true;
     }
 }
